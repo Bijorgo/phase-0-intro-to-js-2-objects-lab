ef45c6ae3c704fb64acda9693e8538672c749818
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-11-01 20:23:00 -0600

    submit objects lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: "P. Sherman",
+    address: "42 Wallaby Way, Sydney",
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    return {
+        ...employee,
+        [key]: value
+    }
+    
+    //use spread operator
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    //should mutate employee object passed in
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee,key) {
+    //spread op.
+    const deleteEmployee = {...employee};
+    delete deleteEmployee[key]
+    return deleteEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    //should mutate
+    delete employee[key]
+    return employee
+}
